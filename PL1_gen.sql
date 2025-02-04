647eab57d4149c5b5b26b9d351d7696e752b5b3b
FerdinandoPH <perez.holguin@gmail.com>
2025-02-04 13:53:25 +0100

    sql para cuestion 1

---
+++ PL1_gen.sql
@@ -0,0 +1,15 @@
+\c postgres
+
+DROP DATABASE IF EXISTS PL1;
+CREATE DATABASE PL1;
+\c pl1
+BEGIN;
+CREATE TABLE productos(
+    producto_id INTEGER PRIMARY KEY,
+    nombre CHAR(50),
+    stock INTEGER,
+    precio INTEGER
+);
+\copy productos FROM 'productos.csv' DELIMITER ',' CSV;
+
+\q
