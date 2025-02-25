664ec2385bb152b8ca2413fb98786aae213793c9
TizianoBR <tizianobiondo@hotmail.com>
2025-02-25 20:24:22 +0100

    c25-26

---
+++ PL1_c25.sql
@@ -0,0 +1,26 @@
+\c pl1
+
+BEGIN;
+
+SELECT pg_stat_reset();
+SET max_parallel_workers_per_gather = 0;
+DISCARD PLANS;
+\o salida.txt
+EXPLAIN (ANALYZE, BUFFERS) SELECT * FROM productos WHERE precio = 1000;
+SELECT * FROM productos WHERE precio = 1000;
+\o
+SHOW max_parallel_workers_per_gather;
+
+COMMIT;
+SELECT pg_sleep(1);
+BEGIN;
+
+SELECT relname, heap_blks_read, heap_blks_hit
+FROM pg_statio_user_tables
+WHERE relname = 'productos';
+
+SELECT *
+FROM pg_statio_user_indexes;
+
+
+COMMIT;
