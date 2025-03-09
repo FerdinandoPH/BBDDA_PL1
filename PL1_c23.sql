3fcefbe44e5af71d042657d1a5622b8ecd9abf56
FerdinandoPH <perez.holguin@gmail.com>
2025-03-09 16:51:19 +0100

    minicambios

--- PL1_c23.sql
+++ PL1_c23.sql
@@ -3,19 +3,19 @@
 BEGIN;
 
 SELECT pg_stat_reset();
-SET max_parallel_workers_per_gather = 0;
+SET max_parallel_workers_per_gather = 0; -- Desactivar paralelismo para asegurarse que las medidas de estadísticas sean consistentes
 DISCARD PLANS;
 
-
+DELETE FROM productos
+WHERE producto_id = 25000001;
 
 -- \o salida.txt
 -- EXPLAIN (ANALYZE, BUFFERS) SELECT * FROM productos WHERE precio = 2000;
 -- SELECT * FROM productos WHERE precio = 2000;
 -- \o
--- SHOW max_parallel_workers_per_gather;
 
 -- COMMIT;
--- SELECT pg_sleep(1);
+-- SELECT pg_sleep(1); -- A veces, no nos salía información en las tablas de estadísticas sin una espera
 -- BEGIN;
 
 -- SELECT relname, heap_blks_read, heap_blks_hit
@@ -102,11 +102,34 @@ DISCARD PLANS;
 
 
 
+-- BEGIN;
+-- SELECT pg_stat_reset();
+-- \o salida.txt
+-- EXPLAIN (ANALYZE, BUFFERS) SELECT precio, COUNT(stock) FROM productos GROUP BY precio ORDER BY precio;
+-- SELECT precio, COUNT(stock) FROM productos GROUP BY precio ORDER BY precio;
+-- \o
+
+
+-- COMMIT;
+-- SELECT pg_sleep(1);
+-- BEGIN;
+
+-- SELECT relname, heap_blks_read, heap_blks_hit
+-- FROM pg_statio_user_tables
+-- WHERE relname = 'productos';
+
+-- SELECT indexrelname, idx_blks_read, idx_blks_hit
+-- FROM pg_statio_user_indexes;
+-- COMMIT;
+
+
+
+
+
 BEGIN;
 SELECT pg_stat_reset();
 \o salida.txt
-EXPLAIN (ANALYZE, BUFFERS) SELECT precio, COUNT(stock) FROM productos GROUP BY precio ORDER BY precio;
-SELECT precio, COUNT(stock) FROM productos GROUP BY precio ORDER BY precio;
+EXPLAIN (ANALYZE, BUFFERS) INSERT INTO productos(producto_id, nombre, stock, precio) VALUES(25000001, 'prod_25000001', 100, 1000);
 \o
 
 
@@ -126,45 +149,22 @@ COMMIT;
 
 
 
--- BEGIN;
--- SELECT pg_stat_reset();
--- \o salida.txt
--- EXPLAIN (ANALYZE, BUFFERS) INSERT INTO productos(producto_id, nombre, stock, precio) VALUES(25000001, 'prod_25000001', 100, 1000);
--- \o
+
+BEGIN;
+SELECT pg_stat_reset();
+\o salida.txt
+EXPLAIN (ANALYZE, BUFFERS) UPDATE productos SET precio = 2000 WHERE producto_id = 25000001;
+\o
 
 
--- COMMIT;
--- SELECT pg_sleep(1);
--- BEGIN;
+COMMIT;
+SELECT pg_sleep(1);
+BEGIN;
 
--- SELECT relname, heap_blks_read, heap_blks_hit
--- FROM pg_statio_user_tables
--- WHERE relname = 'productos';
+SELECT relname, heap_blks_read, heap_blks_hit
+FROM pg_statio_user_tables
+WHERE relname = 'productos';
 
--- SELECT indexrelname, idx_blks_read, idx_blks_hit
--- FROM pg_statio_user_indexes;
--- COMMIT;
-
-
-
-
-
-
--- BEGIN;
--- SELECT pg_stat_reset();
--- \o salida.txt
--- EXPLAIN (ANALYZE, BUFFERS) UPDATE productos SET precio = 2000 WHERE producto_id = 25000001;
--- \o
-
-
--- COMMIT;
--- SELECT pg_sleep(1);
--- BEGIN;
-
--- SELECT relname, heap_blks_read, heap_blks_hit
--- FROM pg_statio_user_tables
--- WHERE relname = 'productos';
-
--- SELECT indexrelname, idx_blks_read, idx_blks_hit
--- FROM pg_statio_user_indexes;
--- COMMIT;
+SELECT indexrelname, idx_blks_read, idx_blks_hit
+FROM pg_statio_user_indexes;
+COMMIT;
